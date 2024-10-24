dda3fccbe3d14bcc3a60194075d95413f1ff183d
thalliumsulfate <thalia.munroe@edu.sait.ca>
2024-10-24 14:05:47 -0600

    Merged in Kouorosh's (Amir) algorithms and Pyramid class
    organized classes

--- src/shapes/Pyramid.java
+++ src/shapes/Pyramid.java
@@ -1,6 +1,37 @@
 package shapes;
 
-public class Pyramid
-{
+// Kourosh (Amir)
 
+public class Pyramid extends Shape {
+    private final double side;
+
+    // GETTER
+    public Pyramid(double side, double height) {
+        super(height);
+        this.side = side;
+    }
+
+    @Override
+    public int compareTo(Shape other) {
+        if (other instanceof Pyramid) {
+            Pyramid otherPyramid = (Pyramid) other;
+            return Double.compare(this.calculateVolume(), otherPyramid.calculateVolume());
+        }
+        return 0; // Or handle other cases as needed
+    }
+
+    // RETURN VOLUME
+
+    @Override
+    public double calculateVolume() {
+        // Volume of a pyramid = (1/3) * base area * height
+        return (1.0 / 3.0) * calculateBaseArea() * height;
+    }
+
+    // Return Area
+    @Override
+    public double calculateBaseArea() {
+        // Base area of a pyramid with a square base = side^2
+        return side * side;
+    }
 }
