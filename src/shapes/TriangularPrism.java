ebc4e1eb8b02dc5342f019839d452e1678c730a6
karman-sait <153486121+karman-sait@users.noreply.github.com>
2024-10-28 05:23:43 -0600

    Update TriangularPrism.java

--- src/shapes/TriangularPrism.java
+++ src/shapes/TriangularPrism.java
@@ -1,6 +1,20 @@
 package shapes;
 
-public class TriangularPrism
-{
+public class TriangularPrism extends Shape {
+    private final double edgeLength;
 
+    public TriangularPrism(double height, double edgeLength) {
+        super(height);
+        this.edgeLength = edgeLength;
+    }
+
+    @Override
+    public double calculateVolume() {
+        return calculateBaseArea() * height;
+    }
+
+    @Override
+    public double calculateBaseArea() {
+        return (Math.sqrt(3) / 4) * edgeLength * edgeLength;
+    }
 }
