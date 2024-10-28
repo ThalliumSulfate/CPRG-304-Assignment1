659b6352d5da4537f5d9b55a0545ced460de55ee
karman-sait <153486121+karman-sait@users.noreply.github.com>
2024-10-28 05:21:56 -0600

    Update Cone.java

--- src/shapes/Cone.java
+++ src/shapes/Cone.java
@@ -1,6 +1,20 @@
 package shapes;
 
-public class Cone
-{
+public class Cone extends Shape {
+    private final double radius;
 
+    public Cone(double height, double radius) {
+        super(height);
+        this.radius = radius;
+    }
+
+    @Override
+    public double calculateVolume() {
+        return (1.0 / 3.0) * calculateBaseArea() * height;
+    }
+
+    @Override
+    public double calculateBaseArea() {
+        return Math.PI * radius * radius;
+    }
 }
