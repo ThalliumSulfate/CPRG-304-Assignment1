2a49cbde92b61bd22d578e78b1da262542a47f3c
karman-sait <153486121+karman-sait@users.noreply.github.com>
2024-10-28 05:22:49 -0600

    Update PentagonalPrism.java

--- src/shapes/PentagonalPrism.java
+++ src/shapes/PentagonalPrism.java
@@ -1,6 +1,20 @@
 package shapes;
 
-public class PentagonalPrism
-{
+public class PentagonalPrism extends Shape {
+    private final double edgeLength;
 
+    public PentagonalPrism(double height, double edgeLength) {
+        super(height);
+        this.edgeLength = edgeLength;
+    }
+
+    @Override
+    public double calculateVolume() {
+        return calculateBaseArea() * height;
+    }
+
+    @Override
+    public double calculateBaseArea() {
+        return (5 * edgeLength * edgeLength * Math.tan(Math.toRadians(54))) / 4;
+    }
 }
